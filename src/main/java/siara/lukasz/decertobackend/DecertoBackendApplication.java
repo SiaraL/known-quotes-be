cea33f143113798b1e5825d298569c21468bc3aa
SiaraL <siaral@github.com>
2021-08-16 02:05:10 +0200

    #1 Initial commit

---
+++ src/main/java/siara/lukasz/decertobackend/DecertoBackendApplication.java
@@ -0,0 +1,14 @@
+package siara.lukasz.decertobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DecertoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecertoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/siara/lukasz/decertobackend/DecertoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package siara.lukasz.decertobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecertoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
